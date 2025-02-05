de8ad083f018beeabd1ebcb2128586cb2b6cb661
BrunaSaNu <bruna.santana.0511@gmail.com>
2025-02-05 13:31:34 -0300

    Implementig method to do a theme

--- src/main/java/com/brusanu/personalblog/controller/TemaController.java
+++ src/main/java/com/brusanu/personalblog/controller/TemaController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -43,6 +44,13 @@ public class TemaController {
 		return ResponseEntity.ok(temaRepository
 				.findAllByDescricaoContainingIgnoreCase(descricao));
 	}
+	
+	@PostMapping
+	public ResponseEntity<Tema> post (@Valid @RequestBody Tema tema){
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(temaRepository.save(tema));
+	}
+	
 	
 	@PutMapping
 	public ResponseEntity<Tema> put(@Valid @RequestBody Tema tema){
