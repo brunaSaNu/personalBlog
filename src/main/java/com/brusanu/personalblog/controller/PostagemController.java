398b62a49bf3513040e80699951a87f61f0896f2
BrunaSaNu <bruna.santana.0511@gmail.com>
2025-02-06 09:18:59 -0300

    Treatment of bad request

--- src/main/java/com/brusanu/personalblog/controller/PostagemController.java
+++ src/main/java/com/brusanu/personalblog/controller/PostagemController.java
@@ -63,10 +63,13 @@ public class PostagemController {
 	
 	@PutMapping
 	public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem){
-		return postagemRepository.findById(postagem.getId())
-				.map(resposta -> ResponseEntity.status(HttpStatus.OK)
-					.body(postagemRepository.save(postagem)))
-				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+		if(postagemRepository.existsById(postagem.getId())) {
+			if(temaRepository.existsById(postagem.getTema().getId()))
+				return ResponseEntity.status(HttpStatus.OK)
+						.body(postagemRepository.save(postagem));
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Ei! este tema não existe :(", null);
+		}
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 	}
 	
 	@ResponseStatus(HttpStatus.NO_CONTENT)
